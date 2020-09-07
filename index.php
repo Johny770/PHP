fb14a0f6645089740fb1001554d9dd7c7f837d82
Johny <43562887+Johny770@users.noreply.github.com>
2020-09-07 15:23:00 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <table border="1" cellpadding="0" cellspacing="0">
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Category</td>
+          <td>Price</td>
+        </tr>
+        <tr>
+          <?
+          echo "<td>$ID</td>";
+          echo "<td>$Name</td>";
+          echo "<td>$Category</td>";
+          echo "<td>$Price</td>";
+          ?>
+        </tr>
+</body>
+</html>
